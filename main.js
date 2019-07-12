f223d62c9f20cddc1d479bc97f0ce5bc2ae832d1
e5580966 <luis.gonzalez@fisglobal.com>
2019-07-12 11:42:53 -0500

    better looking pop up

--- main.js
+++ main.js
@@ -377,7 +377,7 @@ function createPopUp(currentFeature) {
 
   var popup = new mapboxgl.Popup({closeOnClick: true})
       .setLngLat(currentFeature.geometry.coordinates)
-      .setHTML('<h3>' + currentFeature.properties.name +'</h3>' +
+      .setHTML('<h4>' + currentFeature.properties.name +'</h4>' +
           '<button onclick = window.location.assign("http://maps.google.com/maps?saddr=43.077457,-87.882252&daddr=43.090138,-87.976349") type="button" class="btn btn-success">' + 'Go' +
           '</button>'+
           '<button  type="button" class="btn btn-dark">' + 'Menu' +
